5a4ecc977dbcf1c85db264ea39437c1906239355
B1229021 <B1229021@cgu.edu.tw>
2025-08-25 21:27:21 +0800

    mysql

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
 import streamlit as st
+import pymysql
 
-st.title("Hello Streamlit!")
-st.write("這是我的第一個 Streamlit 網頁")
+# 讀取 secrets
+db_config = st.secrets["mysql"]
+
+# 建立連線
+conn = pymysql.connect(
+    host=db_config["mysql_host"],
+    port=int(db_config["mysql_port"]),
+    user=db_config["mysql_user"],
+    password=db_config["mysql_password"],
+    db=db_config["mysql_db"]
+)
+
+# 撈資料
+with conn.cursor() as cursor:
+    cursor.execute("SELECT * FROM your_table_name")  # 替換成你要查的 table
+    rows = cursor.fetchall()
+
+# 顯示在網頁
+st.write("資料庫內容")
+st.dataframe(rows)
